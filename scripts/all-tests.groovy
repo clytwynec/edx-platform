6ef87c025f7e108957cf6a11d87defd7a97d8258
Christine Lytwynec <clytwynec@edx.org>
2015-01-15 13:11:15 -0500

    run unit tests and collect the artifacts

--- scripts/all-tests.groovy
+++ scripts/all-tests.groovy
@@ -1,6 +1,16 @@
 guard {
+  /*
   parallel(
-    job1_1: { build('test-job-one', sha1: params["sha1"], test_spec: "lms/djangoapps/courseware/tests/test_about.py") },
-    job1_2: { build('test-job-one', sha1: params["sha1"], test_spec: "cms/djangoapps/contentstore/management/commands/tests/test_import.py") }
+    lms_unittests: { build('test-job-one', sha1: params["sha1"], test_spec: "lms/djangoapps/courseware/tests/test_about.py") },
+    cms_unittests: { build('test-job-one', sha1: params["sha1"], test_spec: "cms/djangoapps/contentstore/management/commands/tests/test_import.py") }
   )
-} rescue {}
+  */
+
+  lms_unittests = build('test-job-one', sha1: params["sha1"], test_spec: "lms/djangoapps/courseware/tests/test_about.py")
+  cms_unittests = build('test-job-one', sha1: params["sha1"], test_spec: "cms/djangoapps/contentstore/management/commands/tests/test_import.py")
+
+} rescue {
+  def toolbox = extension."build-flow-toolbox"
+  toolbox.slurpArtifacts(lms_unittests)
+  toolbox.slurpArtifacts(cms_unittests)
+}

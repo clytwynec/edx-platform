65b4f1a161295d9bd5b28972b32795ebb356b3c9
Christine Lytwynec <clytwynec@edx.org>
2015-01-15 11:55:11 -0500

    run unit tests in parallel

--- scripts/all-tests.groovy
+++ scripts/all-tests.groovy
@@ -1,4 +1,6 @@
 guard {
-  build('test-job-one', sha1: params["sha1"], SHARD: "shard1")
-  build('test-job-one', sha1: params["sha1"], SHARD: "shard2")
+  parallel(
+    job1_1: { build('test-job-one', sha1: params["sha1"], test_spec: "lms/djangoapps/courseware/tests/test_about.py") },
+    job1_2: { build('test-job-one', sha1: params["sha1"], test_spec: "cms/djangoapps/contentstore/management/commands/tests/test_import.py") }
+  )
 } rescue {}
